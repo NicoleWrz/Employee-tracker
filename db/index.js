4f8fc35cfc50f9247c975fc524a18ae1ed8ab916
NicoleWrz <nicolew96@gmail.com>
2021-12-18 15:59:59 -0500

    updated code

--- db/index.js
+++ db/index.js
@@ -13,9 +13,12 @@ class DB {
       // id, first_name, last_name FROM employee TABLE AND department name from department TABLE AND salary FROM role TABLE
       // YOUR NEED TO USE LEFT JOINS TO JOIN THREE TABLES
       // TODO: YOUR CODE HERE
-      `SELECT employee.id, employee.first_name, employee.last_name, role.salary, department.department_name FROM employee
-      LEFT JOIN role ON role.id = employee.role_id
-      LEFT JOIN department on role.department_id = department.id
+      `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary,
+      CONCAT (manager.first_name, " " , manager.last_name) AS manager
+      FROM employee
+      LEFT JOIN role ON employee.role_id = role.id
+      LEFT JOIN department ON role.department_id = department.id
+      LEFT JOIN employee manager ON manager.id = employee.manager_id
       `
     );
   }
@@ -58,8 +61,9 @@ class DB {
       // id, title, salary FROM role TABLE AND department name FROM department TABLE
       // YOU NEED TO USE LEFT JOIN TO JOIN role and department TABLES
       // TODO: YOUR CODE HERE
-      `SELECT id, title, salary FROM role
-      JOIN department.department_name ON role.department_id = department.id
+      `SELECT role.id, role.title, role.salary, department.name AS department
+      FROM role
+      LEFT JOIN department ON role.department_id = department.id
       `
     );
   }
